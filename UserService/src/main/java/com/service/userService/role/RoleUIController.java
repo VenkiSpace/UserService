e77f6ebd06f5fa8bda7667a2113b6bda3205122d
Venkatesan Sivasubramani <Venkatesan Sivasubramani venkatesan.s@gmail.com>
2022-09-24 23:42:10 +0530

    commit

--- UserService/src/main/java/com/service/userService/role/RoleUIController.java
+++ UserService/src/main/java/com/service/userService/role/RoleUIController.java
@@ -10,8 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.venki.springboot.myfirstwebapp.todo.Todo;
-
 import jakarta.validation.Valid;
 
 @Controller
